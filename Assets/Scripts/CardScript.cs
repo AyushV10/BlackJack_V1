7516871c77697fbeb25179ef695b77d9897eab6a
AyushV10 <av2410new@gmail.com>
2024-05-03 12:45:16 +0530

    First commit

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    public int value = 0;
+
+    public int GetValueOfCard()                       //To get the value of card
+    {
+        return value;
+    }
+
+    public void SetValue(int newValue)               //To set the value of card
+    {
+        value = newValue;
+    }
+
+    public string GetSpriteName()                   //To get the sprite name
+    {
+        return GetComponent<SpriteRenderer>().sprite.name;
+    }
+
+    public void SetSprite(Sprite newSprite)          //To set the sprite name
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+    }
+
+    public void ResetCard()                          //To reset cards 
+    {
+        Sprite back = GameObject.Find("Deck").GetComponent<DeckScript>().GetCardBack();
+        gameObject.GetComponent<SpriteRenderer>().sprite = back;
+        value = 0;
+    }
+}
---
+++ Assets/Scripts/DeckScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class DeckScript : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[53];
+    int currentIndex = 0;
+
+    void Start()
+    {
+        GetCardValues();
+    }
+
+    void GetCardValues()                                 //To get the ccard values
+    {
+        int num = 0;
+        // Loop to assign values to the cards
+        for (int i = 0; i < cardSprites.Length; i++)
+        {
+            num = i;
+            num %= 13;
+            if(num > 10 || num == 0)
+            {
+                num = 10;
+            }
+            cardValues[i] = num++;
+        }
+    }
+
+    public void Shuffle()                                //To shuffle/swap the cards
+    {
+        for(int i = cardSprites.Length -1; i > 0; --i)
+        {
+            int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * cardSprites.Length - 1) + 1;
+            Sprite face = cardSprites[i];
+            cardSprites[i] = cardSprites[j];
+            cardSprites[j] = face;
+
+            int value = cardValues[i];
+            cardValues[i] = cardValues[j];
+            cardValues[j] = value;
+        }
+        currentIndex = 1;
+    }
+
+    public int DealCard(CardScript cardScript)                       //When deal is pressed
+    {
+        cardScript.SetSprite(cardSprites[currentIndex]);
+        cardScript.SetValue(cardValues[currentIndex]);
+        currentIndex++;
+        return cardScript.GetValueOfCard();
+    }
+
+    public Sprite GetCardBack()
+    {
+        return cardSprites[0];
+    }
+}
